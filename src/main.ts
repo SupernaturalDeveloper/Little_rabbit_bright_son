8596ec8b0925b7bb870c0450f3802d7686bd5c45
神马玩意 <2210183204@qq.com>
2022-08-30 15:49:19 +0800

    'distTest'

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { createPinia } from 'pinia';
 import router from './router'
 const store = createPinia();
 // 样式重置
-import './assets/style/reset.css'
+import './assets/style/reset.scss'
 
 createApp(App).use(store).use(router).mount('#app')
