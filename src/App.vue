17129b4dbc938b70d294b708ee3597fe74e1ca09
yuansheng <yuansheng140@163.com>
2022-09-01 17:10:50 +0800

    '完成头部底部2.4'

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
+    <HeaderView/>
     <div>
         <router-view></router-view>
     </div>
+    <FooterTop/>
+    <FooterBottom/>
 </template>
 <script setup lang="ts">
-    import { getFindBanner } from "./api/home";
-    getFindBanner().then((res: any) => console.log(res));
 </script>
 <style lang="scss" scoped>
+
 </style>
