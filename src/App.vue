67d0f201ca155ddb790dca878631fc834e60c443
神马玩意 <2210183204@qq.com>
2022-09-01 20:00:49 +0800

    '解决冲突2.7'

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,6 @@
     <FooterBottom />
 </template>
 <script setup lang="ts">
-    import { getFindBanner } from "./api/home";
-    import { getFindAllCategory } from "./api/category";
-    getFindBanner().then((res: any) => console.log(res));
-    getFindAllCategory().then((res: any) => console.log(res));
 </script>
 <style lang="scss" scoped>
 </style>
